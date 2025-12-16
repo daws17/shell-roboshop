4087a35b48a76ac35f0c25dfe4c29b45f8da970c
sai kiran <saikiranakula17@gmail.com>
2025-12-16 19:23:47 +0530

    instal

--- servers.sh
+++ servers.sh
@@ -6,7 +6,7 @@ SG_ID="sg-00434aea2a5a1cc91"
 for instance in $@
 do
 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-sg-00434aea2a5a1cc91 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]'--query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-sg-00434aea2a5a1cc91 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 
     if [ $instance != "frontend" ]; then
          IP=$(aws ec2 describe-instances --instance-id i-i-03a677ae88b0a425a --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)

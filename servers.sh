fe252fc5eceea7e6e23de47a4c91ca81aad9609b
sai kiran <saikiranakula17@gmail.com>
2025-12-16 22:43:34 +0530

    install

--- servers.sh
+++ servers.sh
@@ -29,12 +29,12 @@ do
              ,"Changes": [{
              "Action"                 : "UPSERT"
             ,"ResourceRecordSet"      : {
-                  "Name"              : "'$RECORD_NAME'"
-                  ,"Type"             : "A"
-                  ,"TTL"              : 1
-                  ,"ResourceRecords"  : [{
+                "Name"              : "'$RECORD_NAME'"
+                ,"Type"             : "A"
+                ,"TTL"              : 1
+                ,"ResourceRecords"  : [{
                        "Value"        : "' $IP '"
-              }]
+                }]
            }
            }]
         }

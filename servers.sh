750947be85a6f796a11b7e75d76bd2d5e93e75cc
sai kiran <saikiranakula17@gmail.com>
2025-12-16 19:51:29 +0530

    instal

--- servers.sh
+++ servers.sh
@@ -9,9 +9,9 @@ do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-sg-00434aea2a5a1cc91 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 
     if [ $instance != "frontend" ]; then
-         IP=$(aws ec2 describe-instances --instance-id -i-03a677ae88b0a425a --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+         IP=$(aws ec2 describe-instances --instance-ids i-03a677ae88b0a425a --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
-         IP=$(aws ec2 describe-instances --instance-id -i-03a677ae88b0a425a --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+         IP=$(aws ec2 describe-instances --instance-ids i-03a677ae88b0a425a --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
          
     fi
 

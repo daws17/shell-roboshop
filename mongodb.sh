44ef9643f6363a082eb4627fa1224e8311bc1682
sai kiran <saikiranakula17@gmail.com>
2025-12-30 18:53:00 +0530

    mongo

--- mongodb.sh
+++ mongodb.sh
@@ -20,10 +20,10 @@ fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e "installing $2....$R FAILURE $N"| tee -a $LOG_FILE
+        echo -e "$2....$R FAILURE $N"| tee -a $LOG_FILE
         exit 1
     else
-        echo -e "installing $2....$G SUCCESS $N"| tee -a $LOG_FILE
+        echo -e "$2....$G SUCCESS $N"| tee -a $LOG_FILE
     fi
 }
 

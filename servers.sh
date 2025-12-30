3603dcbbd90fb90e52508d5a3b2124dc222dab1d
sai kiran <saikiranakula17@gmail.com>
2025-12-30 19:54:05 +0530

    mongo

--- servers.sh
+++ servers.sh
@@ -10,10 +10,10 @@ do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-00434aea2a5a1cc91 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 
     if [ $instance != "frontend" ];then
-         IP=$(aws ec2 describe-instances --instance-ids i-03a677ae88b0a425a --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+         IP=$(aws ec2 describe-instances --instance-ids i-0fe78974721686639 --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
          RECORD_NAME="$INSTANCE.$DOMAIN_NAME"
     else
-         IP=$(aws ec2 describe-instances --instance-ids i-03a677ae88b0a425a --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+         IP=$(aws ec2 describe-instances --instance-ids i-0fe78974721686639 --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
          RECORD_NAME="$DOMAIN_NAME"
          
     fi

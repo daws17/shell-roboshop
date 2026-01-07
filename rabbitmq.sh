85eb86515887ab2324abf180be2d00f01fa92987
sai kiran <saikiranakula17@gmail.com>
2026-01-08 00:52:24 +0530

    rabbitmq

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -8,7 +8,7 @@ N="\e[34m"
 
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-SCRIPT_DIR=$PWD
+SCRIPT_DIR=($PWD)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 START_TIME=$(date +%s)
 

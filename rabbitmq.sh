fe5387d46c05feb884fb134f6f31e50b43154dcd
sai kiran <saikiranakula17@gmail.com>
2026-01-08 00:37:31 +0530

    validate function added to repo

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -30,6 +30,7 @@ VALIDATE(){
 }
 
 CP $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>> $LOG_FILE
+VALIDATE $? "adding rabbitmq repo"
 
 dnf install rabbitmq-server -y &>> $LOG_FILE
 VALIDATE $? "installing rabbitmq server"

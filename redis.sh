0f7081b795874f5d096e7a9f207844abd3aeeb1a
sai kiran <saikiranakula17@gmail.com>
2026-01-07 23:49:49 +0530

    validate function in redis

--- redis.sh
+++ redis.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-set -euo pipefail
-
-trap 'echo "there is error in $LINENO, command is: $BASH_COMMAND"' ERR
-
-
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -23,16 +18,29 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi
 
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2....$R FAILURE $N"| tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2....$G SUCCESS $N"| tee -a $LOG_FILE
+    fi
 
 
 dnf module disable redis -y &>> $LOG_FILE
+VALIDATE $? "disabling redis"
 
 dnf module enable redis:7 -y &>> $LOG_FILE
+VALIDATE $? "enabling redis 7"
 
 dnf install redis -y &>> $LOG_FILE
+VALIDATE $? "installing redis 7"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e /protected-mode/ c protected-mode no /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "allowing remote connections to redis"
 
 systemctl enable redis &>> $LOG_FILE
+VALIDATE $? "enabling redis"
 
 systemctl start redis &>> $LOG_FILE
+VALIDATE $? "starting redis"

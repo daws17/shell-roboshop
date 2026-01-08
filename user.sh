b92ebc8bc81d08c4ee921d36783b0c31df1ad0ed
sai kiran <saikiranakula17@gmail.com>
2026-01-08 19:46:32 +0530

    user is checking

--- user.sh
+++ user.sh
@@ -24,14 +24,7 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi
 
-VALIDATE(){
-    if [ $1 -ne 0 ]; then
-        echo -e "$2....$R FAILURE $N"| tee -a $LOG_FILE
-        exit 1
-    else
-        echo -e "$2....$G SUCCESS $N"| tee -a $LOG_FILE
-    fi
-}
+
 
 dnf module disable nodejs -y
 
